0bfd772cd4e924311bd6a1178f2b6197969f000f
unknown <cesarrcosta99@gmail.com>
2022-11-29 17:41:44 -0300

    Exercicio 2 e 3 FEITO

---
+++ exercicio2/script.js
@@ -0,0 +1,9 @@
+const array=[1,2,3,4,5,6,7,8]
+
+const triplos=array.map((elemento)=> {
+  return elemento * 3
+})
+ console.log(triplos)
+
+ const metades=array.map((elemento)=>elemento /2)
+ console.log(metades)
